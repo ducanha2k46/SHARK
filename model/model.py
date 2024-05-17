b6ff482991d25bfd39ef73532fc7eacc8df1eabe
duc_anh <ducanha2k46@gmail.com>
2024-05-17 20:23:09 +0700

    change subtask label

--- model/model.py
+++ model/model.py
@@ -30,7 +30,8 @@ class GuidedMoEBasic(nn.Module):
     def binary_cause_classification_task(self, emotion_prediction, input_ids, h_prime, speaker_ids):
         pair_embedding = self.get_pair_embedding(emotion_prediction, input_ids, h_prime, speaker_ids)
         gating_prob = self.gating_network(pair_embedding.view(-1, pair_embedding.shape[-1]).detach())
-
+        print("gating_prob.size()")
+        print(gating_prob.size())
         gating_prob = self.guiding_lambda * self.get_subtask_label(input_ids, speaker_ids, emotion_prediction).view(-1, self.n_expert) + (1 - self.guiding_lambda) * gating_prob
 
         pred = []
@@ -47,24 +48,24 @@ class GuidedMoEBasic(nn.Module):
         return self.gating_network(pair_embedding.view(-1, pair_embedding.shape[-1]).detach())
 
     def get_pair_embedding(self, emotion_prediction, input_ids, h_prime, speaker_ids):
-        batch_size, max_doc_len = input_ids.size()
-        max_doc_len = batch_size
+        max_doc_len, x = input_ids.size()
+        batch_size = 1
         print(input_ids.size())
         print(emotion_prediction.size())
         print(h_prime.size())
         
        
         emotion_prediction = torch.mean(emotion_prediction, dim = 1)
-        h_prime =  torch.mean(emotion_prediction, dim = 1)
+        h_prime =  torch.mean(h_prime, dim = 1)
         print(input_ids.size())
         print(emotion_prediction.size())
         print(h_prime.size())
+        print(speaker_ids.view(-1).unsqueeze(1).size())
        
-       
-        concatenated_embedding = torch.cat((h_prime, emotion_prediction, input_ids), dim=1) # 여기서 emotion_prediction에 detach를 해야 문제가 안생기겠지? 해보고 문제생기면 detach 고고
+        concatenated_embedding = torch.cat((h_prime, emotion_prediction, speaker_ids.unsqueeze(1)), dim=1) # 여기서 emotion_prediction에 detach를 해야 문제가 안생기겠지? 해보고 문제생기면 detach 고고
 
         pair_embedding = list()
-        for batch in concatenated_embedding.view(batch_size,max_doc_len -1):
+        for batch in concatenated_embedding.view(batch_size, max_doc_len, -1):
             pair_per_batch = list()
             for end_t in range(max_doc_len):
                 for t in range(end_t + 1):
@@ -72,7 +73,8 @@ class GuidedMoEBasic(nn.Module):
             pair_embedding.append(torch.stack(pair_per_batch))
         
         pair_embedding = torch.stack(pair_embedding).to(input_ids.device)
-
+        print("pair_embedding.size()")
+        print(pair_embedding.size())
         return pair_embedding
 
     def get_subtask_label(self, input_ids, speaker_ids, emotion_prediction):
@@ -83,20 +85,61 @@ class PRG_MoE(GuidedMoEBasic):
     def __init__(self, dropout=0.5, n_speaker=2, n_emotion=7, n_cause=2, n_expert=4, guiding_lambda=0, **kwargs):
         super().__init__(dropout=dropout, n_speaker=n_speaker, n_emotion=n_emotion, n_cause=n_cause, n_expert=4, guiding_lambda=guiding_lambda)
 
+    # def get_subtask_label(self, input_ids, speaker_ids, emotion_prediction):
+    #     most_common_numbers = []
+    #     probabilities = torch.argmax(emotion_prediction, dim=-1).cpu().numpy()
+    #     for i, sentence in enumerate(probabilities):
+    #         num_characters = speaker_ids[i]
+            
+    #         counts = np.bincount(sentence[:num_characters])
+            
+    #         sorted_counts = sorted(enumerate(counts), key=lambda x: x[1], reverse=True)
+            
+    #         most_common = 0
+    #         if sorted_counts[0][0] == 0:
+    #             if len(sorted_counts) > 1:
+    #                 most_common = sorted_counts[1][0]
+    #         else:
+    #             most_common = sorted_counts[0][0]
+            
+    #         most_common_numbers.append(most_common)
+        
+    #     print(most_common_numbers)
+        
+    #     pair_info = []
+    #     for i in range(len(input_ids)):
+    #         for j in range(i + 1, len(input_ids)):
+    #             if (i % 2 == j % 2):  
+    #                 speaker_same = True
+    #             else:
+    #                 speaker_same = False
+    #             emotion_same = most_common_numbers[i] == most_common_numbers[j]  
+    #             if speaker_same and emotion_same:
+    #                 pair_info.append(torch.tensor([1, 0, 0, 0]))
+    #             elif speaker_same and not emotion_same:
+    #                 pair_info.append(torch.tensor([0, 1, 0, 0]))
+    #             elif not speaker_same and emotion_same:
+    #                 pair_info.append(torch.tensor([0, 0, 1, 0]))
+    #             else:
+    #                 pair_info.append(torch.tensor([0, 0, 0, 1]))
+
+    #     pair_info = torch.stack(pair_info).to(input_ids.device)
+    #     print("pair_info:")
+    #     print(pair_info.size())
+    #     return pair_info
+
     def get_subtask_label(self, input_ids, speaker_ids, emotion_prediction):
+        batch_size = 1
+        max_doc_len, x= input_ids.size()
         most_common_numbers = []
         probabilities = torch.argmax(emotion_prediction, dim=-1).cpu().numpy()
         for i, sentence in enumerate(probabilities):
-                # Lấy số kí tự của câu thứ i
             num_characters = speaker_ids[i]
             
-            # Tính toán số lần xuất hiện của mỗi số trong câu
             counts = np.bincount(sentence[:num_characters])
             
-            # Sắp xếp theo số lần xuất hiện giảm dần
             sorted_counts = sorted(enumerate(counts), key=lambda x: x[1], reverse=True)
             
-            # Lấy số xuất hiện nhiều nhất (nếu không phải là 0) hoặc số thứ hai (nếu giá trị nhiều nhất là 0)
             most_common = 0
             if sorted_counts[0][0] == 0:
                 if len(sorted_counts) > 1:
@@ -106,25 +149,28 @@ class PRG_MoE(GuidedMoEBasic):
             
             most_common_numbers.append(most_common)
         
-        print(most_common_numbers)
-        
         pair_info = []
-        for i in range(len(input_ids)):
-            for j in range(i + 1, len(input_ids)):
-                if (i % 2 == j % 2):  
-                    speaker_same = True
-                else:
-                    speaker_same = False
-                emotion_same = most_common_numbers[i] == most_common_numbers[j]  
-                if speaker_same and emotion_same:
-                    pair_info.append(torch.tensor([1, 0, 0, 0]))
-                elif speaker_same and not emotion_same:
-                    pair_info.append(torch.tensor([0, 1, 0, 0]))
-                elif not speaker_same and emotion_same:
-                    pair_info.append(torch.tensor([0, 0, 1, 0]))
-                else:
-                    pair_info.append(torch.tensor([0, 0, 0, 1]))
+        for speaker_batch, emotion_batch in zip(speaker_ids.view(batch_size, max_doc_len, -1), emotion_prediction.view(batch_size, max_doc_len, -1)):
+            info_pair_per_batch = []
+            for end_t in range(max_doc_len):
+                for t in range(end_t + 1):
+                    if (end_t % 2 == t % 2):  
+                        speaker_condition = True
+                    else:
+                        speaker_condition = False
+                    emotion_condition = most_common_numbers[t] == most_common_numbers[end_t]
 
+                    if speaker_condition and emotion_condition:
+                        info_pair_per_batch.append(torch.Tensor([1, 0, 0, 0])) # if speaker and dominant emotion are same
+                    elif speaker_condition:
+                        info_pair_per_batch.append(torch.Tensor([0, 1, 0, 0])) # if speaker is same, but dominant emotion is differnt
+                    elif emotion_condition:
+                        info_pair_per_batch.append(torch.Tensor([0, 0, 1, 0])) # if speaker is differnt, but dominant emotion is same
+                    else:
+                        info_pair_per_batch.append(torch.Tensor([0, 0, 0, 1])) # if speaker and dominant emotion are differnt
+            pair_info.append(torch.stack(info_pair_per_batch))
+        
         pair_info = torch.stack(pair_info).to(input_ids.device)
-
+        print("pair_info")
+        print(pair_info.size())
         return pair_info
